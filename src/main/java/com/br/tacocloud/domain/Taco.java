69444f7c52627ca52243748632228151a43710cd
vitor tavares <vitortavares984@gmail.com>
2022-06-07 21:20:49 -0300

    Chapter 01 commited
    This commit contains initial html pages, initial configurations and Controller

---
+++ src/main/java/com/br/tacocloud/domain/Taco.java
@@ -0,0 +1,11 @@
+package com.br.tacocloud.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
+}
